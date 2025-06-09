d9151bc01e5f4b29bddeed39feccd0ef3284446d
Tuğba KARAMAN <97016474+KaramanTugba@users.noreply.github.com>
2025-06-09 18:56:33 +0300

    DoWhile_While

---
+++ Week2/WhileVSDoWhile/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace WhileVSDoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Bir sayı giriniz:. ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            // Console.WriteLine(a);
+
+            int sayac = 0;
+            Console.WriteLine("While");
+            while (sayac <= a )
+            {
+                Console.WriteLine("Ben bir Patika'lıyım");
+                sayac++;
+            }
+
+            Console.WriteLine("--------------------------------------------");
+
+            Console.WriteLine("Do While");
+            sayac = 0;
+            do
+            {
+                Console.WriteLine("Ben bir Patika'lıyım");
+                sayac++;
+            } while (sayac <= a);
+
+
+        }
+    }
+}
