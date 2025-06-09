38a4a5b55abe0ecb2aa91ee66082d791f065f8f8
Tuğba KARAMAN <97016474+KaramanTugba@users.noreply.github.com>
2025-06-09 16:30:30 +0300

    If_Else
    Pratik eklendi

---
+++ Week1_/If_Else/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace If_Else
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Sayı giriniz: ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+
+            if (sayi > 10)
+            {
+                Console.WriteLine("Girilen sayi 10 dan büyüktür");
+            }
+            else
+            {
+                    
+                Console.WriteLine("Girilen sayi 10 a eşit yada küçüktür.");
+            }
+
+
+            if (sayi % 2 == 0 )
+            {
+                Console.WriteLine("Girilen sayi çifttir");
+            }
+            else
+            {
+
+                Console.WriteLine("Girilen sayi tektir");
+            }
+
+
+
+
+        }
+    }
+}
