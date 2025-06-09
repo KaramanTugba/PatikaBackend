9dc70dbbfafe143cab916a6d02c963960d6a1a47
Tuğba KARAMAN <97016474+KaramanTugba@users.noreply.github.com>
2025-06-09 15:30:16 +0300

    Week1 _
    İlk uygulamam yüklendi

---
+++ Week1_/Week1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Week1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tuğba Karaman");
+            Console.WriteLine("Sektörde iyi bir konumda olmak istediğim için bu eğitimi alıyorum.");
+        }
+    }
+}
